06fe6c1d7f457aa0b2e5c29bcb854ac99d1b738e
haythemjfzz <berkanihaythem53@gmail.com>
2025-06-28 06:49:29 -0700

    app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def submit_data():
     print("Received Data:", data)  # Debugging output
 
     # Check the value of s_science_week and send data accordingly
-    if data.get("s_science_week") == "1":
+    if data.get("currentDepartment") == "s":
         response = iPlan_Sotfwar_S.process_data(data)
     
     else:
