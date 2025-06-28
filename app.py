ee39a23de61c688ecc638875112b3d97fc1e2534
haythemjfzz <berkanihaythem53@gmail.com>
2025-06-28 05:42:07 -0700

    app.py

--- app.py
+++ app.py
@@ -11,32 +11,12 @@ def submit_data():
     data = request.json  # Receive JSON data
     print("Received Data:", data)  # Debugging output
 
-    # Check the value of currentDepartment and send data accordingly
-    if data.get("currentDepartment") == "1":
+    # Check the value of s_science_week and send data accordingly
+    if data.get("s_science_week") == "1":
         response = iPlan_Sotfwar_S.process_data(data)
-    elif data.get("currentDepartment") == "2":
-        response = iPlan_Sotfwar_M.process_data(data)
-    elif data.get("currentDepartment") == "3":
-        response = iPlan_Sotfwar_MTGM.process_data(data)
-    elif data.get("currentDepartment") == "4":
-        response = iPlan_Sotfwar_MTGE.process_data(data)
-    elif data.get("currentDepartment") == "5":
-        response = iPlan_Sotfwar_MTGC.process_data(data)
-    elif data.get("currentDepartment") == "6":
-        response = iPlan_Sotfwar_MTGP.process_data(data)
-    elif data.get("currentDepartment") == "7":
-        response = iPlan_Sotfwar_G.process_data(data)
-    elif data.get("currentDepartment") == "8":
-        response = iPlan_Sotfwar_L.process_data(data)
-    elif data.get("currentDepartment") == "9":
-        response = iPlan_Sotfwar_LES.process_data(data)
-    elif data.get("currentDepartment") == "10":
-        response = iPlan_Sotfwar_LEG.process_data(data)
-
-    
     
     else:
-        response = {"error": "Invalid currentDepartment value"}
+        response = {"error": "Invalid s_science_week value"}
 
     
 
